7aca8dee62087a7350bba6d09729f3947d56b894
andreyras <115710211+andreyras@users.noreply.github.com>
2022-11-18 20:14:21 +0700

    Add files via upload

---
+++ даадад.py
@@ -0,0 +1,14 @@
+comand = input("Введите команду").lower()
+
+while comand != "start":
+    if comand == "погода":
+        print("Введите город")
+    if comand == "Новосибирск"
+        print("Погода Новосибирск: -10 градусов утром, -8 днём, -11 вечером, -13 ночью.")
+        print("")
+    elif comand == "краборифма":
+        slovo = input("Введите любое слово")
+        print("крабо"+slovo)
+    
+    command = input("Введите команду").lower()
+print("Чат-бот закончил")
