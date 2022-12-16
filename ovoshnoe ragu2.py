ffce80cecb57bf735d4b18fac9aff484b5dd47ab
andreyras <115710211+andreyras@users.noreply.github.com>
2022-12-16 20:13:26 +0700

    Add files via upload

---
+++ ovoshnoe ragu2.py
@@ -0,0 +1,7 @@
+ovoshi=["pomidor", "kapycta","baklazhan", "ogyrec"]
+print("dobav eshe ovosh")
+a1=input("vpishi ovosh:")
+ovoshi.append(a1)
+a2=input("vpishi eshe ovosh:")
+ovoshi.append(a2)
+print(ovoshi)
