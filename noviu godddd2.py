c2e2f9f3227a7b450f25bd954db26fe40352c6f3
andreyras <115710211+andreyras@users.noreply.github.com>
2022-12-23 19:24:32 +0700

    Add files via upload

---
+++ noviu godddd2.py
@@ -0,0 +1,13 @@
+a=input("s novim godom")
+if a == "s novim godom tozh" :
+    print("go olivie poxavaem")
+if a == "woow maan thats coo00l1" :
+    print("wow english , you know")
+if a == "a ti znal chto noviy god tolko cherez nedelyu?" :
+    print("chego")
+if a == "ti kto" :
+    print("shiza chel, shiza")
+if a == "zaxodit ulytka v bar..." :
+    print("bayan + ne v temy + ne po novogodnemy") 
+else:
+    print("cho")
