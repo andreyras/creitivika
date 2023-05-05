a7d2ec065ce550071c1bd48ee2058351b37bc748
andreyras <115710211+andreyras@users.noreply.github.com>
2023-05-05 19:00:40 +0700

    Add files via upload

---
+++ code.py
@@ -0,0 +1,11 @@
+import telebot
+bot = telebot.TeleBot('6047924096:AAGFRLTu_9uq7lc3BKX2s6bpNLFwZa3wVdw')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
